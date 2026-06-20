b04a5c1f685e86f87bebcce05490631798db5c10
Akrit Chauhan <156348636+Akritchauhan@users.noreply.github.com>
2026-06-20 22:26:54 +0530

    Refactor generator class and update syntax

--- generator.sv
+++ generator.sv
@@ -1,35 +1,88 @@
-class generator
-  bit random_mode;
-  
-  transaction user_queue[$];
-  mailbox gen2drv;
-  int num_transaction=10;
-  
-  function new(mailbox gen2drv);
-    this.gen2drv=gen2drv;
-    this.random_mode=1;
-  endfunction
-  
-  task add_user_transaction(transaction tr);
-    user_queue_push_back(tr);
-  endtask
-  
-  task run();
-    transaction tr;
-    if(random_mode)begin
-      $display("[Gen] running random mode");
-      repeat(num_transaction)begin
-        tr=new();
-        assert(tr.randomize() with{(wr_en || rd_en);});
-        tr.display("Gen");
-        gen2drv.put(tr);
-      end
-    end else begin
-      $display("[Gen] Running in user-defined mode");
-      foreach(user_queue[i])begin
-        user_queue[i].display("GEN");
-        gen2drv.put(user_queue[i]);
-      end
-    end
-  endtask
+`include "transaction.sv"
+
+class generator;
+
+
+bit random_mode;
+
+transaction user_queue[$];
+
+mailbox gen2drv;
+
+int num_transaction=10;
+
+
+
+function new(mailbox gen2drv);
+
+this.gen2drv=gen2drv;
+random_mode=1;
+
+endfunction
+
+
+
+task add_user_transaction(transaction tr);
+
+user_queue.push_back(tr);
+
+endtask
+
+
+
+task run();
+
+
+transaction tr;
+
+
+if(random_mode)
+
+begin
+
+$display("[GEN] Random Mode");
+
+
+repeat(num_transaction)
+
+begin
+
+tr=new();
+
+
+assert(tr.randomize() with {wr_en || rd_en;});
+
+
+tr.display("GEN");
+
+
+gen2drv.put(tr);
+
+
+end
+
+end
+
+
+
+else
+
+begin
+
+
+foreach(user_queue[i])
+
+begin
+
+gen2drv.put(user_queue[i]);
+
+end
+
+
+end
+
+
+endtask
+
+
 endclass
